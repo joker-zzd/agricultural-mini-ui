6184186a601442fb6d16deb74d0dd224f3498ebd
huangxianjun <1925638055@qq.com>
2025-06-04 10:50:43 +0800

    feat：开放IP地址

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { VantResolver } from "unplugin-vue-components/resolvers";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: "0.0.0.0",
     port: 7061,
   },
   plugins: [
