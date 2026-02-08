e16701340ba78951b7ca9ac6e005387af97e8198
19256 <192563855@qq.com>
2026-02-09 00:45:29 +0800

    fix: 配置文件修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "node:path";
 import Components from "unplugin-vue-components/vite";
 import { VantResolver } from "unplugin-vue-components/resolvers";
 
